266972ce830f571522e4a0196a51e90be4695af1
Adam McCarthy <adam@intercom.io>
2013-01-17 14:25:09 +0000

    Initial commit.

---
+++ Intercom/Intercom.h
@@ -0,0 +1,132 @@
+//
+//  Intercom.h
+//  Intercom
+//
+//  Copyright (c) 2013 Intercom, Inc. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#import <Foundation/Foundation.h>
+
+/** This is the interface for the Intercom iOS SDK.  From here you can begin and end sessions, track, update or increment attributes 
+ that you can use to create filters and segments out of on the web.
+
+ @note This product is currently in beta as we add more features to it.
+ */
+
+@interface Intercom : NSObject
+
+//=========================================================================================================
+/** @name Connecting to Intercom. */
+//=========================================================================================================
+/*! 
+ Prior to sending any data to Intercom, the SDK must be initialized with a valid `apiKey` and `appId`.  
+ These can be found in the configuration section of your application's 
+ settings page. 
+ 
+ This should be placed in the iOS application delegate like so;
+ 
+    - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:
+                         (NSDictionary *)launchOptions
+    {
+        // Override point for customization after application launch
+        [Intercom setApiKey:@"my_api_key" forAppId:@"my_app_id"];
+        return YES;
+    }
+ 
+ @param apiKey The api key required to communicate with your Intercom application.
+ @param appId The app id of your Intercom application.
+ */
+
++ (void)setApiKey:(NSString *)apiKey forAppId:(NSString *)appId;
+
+//=========================================================================================================
+/** @name Session control. */
+//=========================================================================================================
+/*!
+ Begins a session for the specified user.  Either an email address or a unique identifier must be used.  
+ This should be the same as the one used for your web application.
+ @note Sessions only need to begin on authentication success.  Intercom listens for `UIApplication` state 
+ changes so endSession doesn't need to be implemented in any additional locations other than a logout method.
+ @param email The user's email address.
+ */
++ (void)beginSessionForUserWithEmail:(NSString *)email;
+
+/*!
+ Begins a session for the specified user but using a unique identifier rather than an email address.
+ @note Sessions only need to begin on authentication success.  Intercom listens for `UIApplication` state
+ changes so endSession doesn't need to be implemented in any additional locations other than a logout method.
+ @param userId A unqiue identifier representing the user..
+ */
++ (void)beginSessionForUserWithUserId:(NSString *)userId;
+
+/*!
+ Begins a session for the specified user with both an email address and a unique identifier.  Only use this
+ method if both pieces of information are present.
+ @note Sessions only need to begin on authentication success.  Intercom listens for `UIApplication` state
+ changes so endSession doesn't need to be implemented in any additional locations other than a logout method.
+ @param userId A unqiue identifier representing the user..
+ @param email The user's email address.
+ */
++ (void)beginSessionForUserWithUserId:(NSString *)userId andEmail:(NSString *)email;
+
+/*!
+ Ends a session for a user. Typically used while logged a user out. All other changes of application state
+ are managed for you so there is no need to implement this in your application's delegate.
+ @note Only implement endSession when logging a user out of your application.  You will not need to implement it
+ anywhere else as Intercom listens for changes in UIApplication state and calculates sessions based on those actions.
+ */
++ (void)endSession;
+
+//=========================================================================================================
+/** @name Working with attributes. */
+//=========================================================================================================
+/*!
+ Incrementing an attribute will add a value of `1` to the key of your choice. 
+ 
+ In the example below, the attribute `sent_support_request` is incremented by a value of `1` every time.
+ 
+    [Intercom incrementAttribute:@"sent_support_request"];
+ 
+ @param attribute The name for which the attribute will be recorded under.
+ */
++ (void)incrementAttribute:(NSString *)attribute;
+
+/*!
+ Updating attributes allows for the submission of multiple attributes with custom values. Attribute values can 
+ also be decremented by using a negative operator.
+
+ For example, the following dictionary can be sent;
+
+    [Intercom updateAttributes:@{ @"increments" : @{ @"made_in_app_purchase" : @1 }, @"last_purchase_date" : @12345678 }];
+ 
+ Where `made_in_app_purchase` is incremented with a value of 1 and `last_purchase_date` is simply updated with
+ the timestamp `12345678`. 
+ 
+ @param attributes This is a dictionary containing key/value pairs for multiple attributes.
+ @warning Attributes may be either a `string`, `integer`, `double`, `unix timestamp` or `bool`.
+ @note Note that to set custom incremental values, the syntax is { @"increments" : @{ @"made_in_app_purchase" : @1 } }
+ 
+ */
++ (void)updateAttributes:(NSDictionary *)attributes;
+
+//=========================================================================================================
+/** @name Updating a user's name. */
+//=========================================================================================================
+/*!
+ Append the user's full name so that it appears under their profile in Intercom.
+ @param fullName The user's name.
+ */
++ (void)updateUserName:(NSString *)fullName;
+
+@end
---
+++ README.md
@@ -0,0 +1,65 @@
+# Intercom iOS SDK [Beta]
+**Intercom iOS SDK**  instructions **rough draft**.
+This is a **beta** release of the Intercom iOS SDK.  As such it is subject to modification without notice.
+
+##Installation
+---
+Copy `Intercom.h` and `libIntercom.a` to your Xcode project and add be sure to include `Intercom.h` in your precompiled header (.pch) file. The library is a fat binary, so will work on both the iOS simulator and devices.  
+
+###Initialize Intercom
+
+In order to initialize Intercom in your iOS app, copy your iOS API key and App ID  from [here](https://www.intercom.io/apps/tx2p130c/api_keys) and add the following line to your application delegate:
+
+```
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    // Override point for customization after application launch
+    [Intercom setApiKey:@"API-KEY" forAppId:@"APP-ID"];
+    
+    return YES;
+}
+```
+
+###Begin a session
+Intercom only tracks users who are logged in to your app.  With this in mind, you should begin a session for a user at the point in your app where authentication is confirmed.  Don't worry if Intercom hasn't seen a user before, that user will be created in Intercom when the session begins.
+
+There are three ways to begin a session, depending on the information available to you.
+
+1. **Email address.**  You can begin a session with a user's email address simply by using the following method at the point where your app successfully authenticates a user;   
+`[Intercom beginSessionForUserWithEmail:adam@intercom.io];`
+2. **UserId.** The same can be accomplished if you don't have a user's email address but you might have another unique identifier you are already sending to Intercom.  If this is the case, you could use the following;  
+`[Intercom beginSessionForUserWithUserId:@"12345"];`
+3. **Both.** Should you have both an ID you would like to couple with an email address, you can send them both;  
+`[Intercom beginSessionForUserWithUserId:@"12345" andEmail:@"adam@intercom.io"]`
+
+Once a session has begun, Intercom will track UIApplication state changes for you, so you won't need to explicity start and stop a session. Start on login and end on logout, we'll do the rest.
+	
+###End a session
+End a session when your user successfully logs out of your application by adding `[Intercom endSession]` at that point.
+
+##Working with attributes
+---
+You can send any data you like to Intercom. Typically our customers see a lot of value in sending data that relates to customer development, such as price plan, value of purchases, etc. Once these have been sent to Intercom you can then apply filters based on these attributes. Full details are available [here](http://docs.intercom.io/intercom-ios/Classes/Intercom.html#task_Working%20with%20attributes).
+
+####Incrementing attributes
+Attributes can be simply sent as strings that are auto incremented for you. The example below will add a value of `1` to the attribute `sent_support_request` every time it is received.
+
+`[Intercom incrementAttribute:@"sent_support_request"];`
+
+You do not have to create attributes in Intercom before hand. If one hasn't been seen before, it will be created for you.
+
+####Custom attributes
+You can send a `NSDictionary` containing multiple attributes also. Note that attribute values may be either a `string`, `integer`, `double`, `unix timestamp` or `bool`. Note the syntax of incremental attributes in the following example:
+
+`[Intercom updateAttributes:@{ @"increments" : @{ @"made_in_app_purchase" : @1 }, @"last_purchase_date" : @12345678 }];`
+
+
+##Documentation
+---
+Further details are available in the [documentation for the iOS SDK](http://docs.intercom.io/intercom-ios/Classes/Intercom.html).
+
+##License
+---
+Licensed under the Apache License, Version 2.0. Copyright (c) 2013 Intercom, Inc. All rights reserved.
+
+![Smaller](https://d2nvy5xhcrv86v.cloudfront.net/assets/emails/v01/mail-logo-143d4547a151e7cb9e1571a633ecbb4b.png)
