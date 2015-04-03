9118b846362b7365b7ee0e3ac28c6ae8cc20287f
Adam McCarthy <adam@intercom.io>
2015-04-03 10:31:55 +0100

    Version 2.2.1
    * Added support for closing the SDK’s UI when calling `+setMessagesHidden:` and `+reset`. If any part of the SDK is open when these are called, it will now close.
    * Fixed #52: Removed firstResponder swizzle completely, fixing https://github.com/intercom/intercom-ios/issues/52.
    * Fixed #58: Checking for isValidJSONObject on user attributes and event metadata, fixing https://github.com/intercom/intercom-ios/issues/58.

--- Intercom.framework/Versions/A/Headers/Intercom.h
+++ Intercom.framework/Versions/A/Headers/Intercom.h
@@ -11,7 +11,7 @@
 #import <UIKit/UIKit.h>
 
 #if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_7_0
-#error This version (2.2) of the Intercom iOS SDK supports iOS 7.0 upwards.
+#error This version (2.2.1) of the Intercom iOS SDK supports iOS 7.0 upwards.
 #endif
 
 // Use these values to constrain an incoming notification view to a defined section of the window.
@@ -31,37 +31,37 @@ typedef NS_ENUM(NSUInteger, ICMPreviewPosition){
  In order to see your users in Intercom's user list, you must first register them via your iOS application. If you have a
  place in your application where you become aware of the user's identity such as a log in view controller, call one of the
  following depending on the information you have available for that user:
-
+ 
  If you have both a unique user identifier and an email for your users::
  
-    [Intercom registerUserWithUserId:@"123456" email:@"joe@example.com"];
+ [Intercom registerUserWithUserId:@"123456" email:@"joe@example.com"];
  
  If you only have a unique identifier for your users:
  
-    [Intercom registerUserWithUserId:@"123456"];
+ [Intercom registerUserWithUserId:@"123456"];
  
  Finally, if you only have an email address for your users:
-    
-    [Intercom registerUserWithEmail:@"joe@example.com"];
+ 
+ [Intercom registerUserWithEmail:@"joe@example.com"];
  
  ## Can I track unidentified users?
  
  Yes, absolutely. If you have an application that doesn't require users to log in, you can call:
  
-    [Intercom registerUnidentifiedUser];
+ [Intercom registerUnidentifiedUser];
  
- If the user subsequently logs in or you learn additional information about them (e.g. get an email address), 
- calling any of the other user registration methods will update that user's identity in Intercom and contain 
+ If the user subsequently logs in or you learn additional information about them (e.g. get an email address),
+ calling any of the other user registration methods will update that user's identity in Intercom and contain
  all user data tracked previously.
  
  ## I'm using a previous SDK version and this looks different, what has changed?
  
  We have re-architected the internals of the iOS SDK to ensure it is as reliable as possible while tracking
  your users. We have focused on removing the asynchronous behaviour of the SDK. For example you no longer need
- to wait for the completion blocks of the old `beginSession` calls before logging events or updating user data. 
+ to wait for the completion blocks of the old `beginSession` calls before logging events or updating user data.
  In doing so the SDK is more nimble and reliable than ever before.
  
- Previous versions of the SDK will migrate with minimal effort. All deprecated methods still work for now, 
+ Previous versions of the SDK will migrate with minimal effort. All deprecated methods still work for now,
  excluding the old session listener (since v2.0.6). These methods will be permanently removed in a future
  version.
  
@@ -69,11 +69,11 @@ typedef NS_ENUM(NSUInteger, ICMPreviewPosition){
  
  The Intercom iOS SDK enables your users to receive push notifications for new messages. Simply call:
  
-    - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-        [Intercom setDeviceToken:deviceToken];
-    }
+ - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+ [Intercom setDeviceToken:deviceToken];
+ }
  
- in your `didRegisterForRemoteNotificationsWithDeviceToken:` method once you have registered your app for 
+ in your `didRegisterForRemoteNotificationsWithDeviceToken:` method once you have registered your app for
  push notifications with the `UIApplicationDelegate`.
  
  When your app receives a push notification the SDK checks to see if it is an Intercom push notification
@@ -84,7 +84,7 @@ typedef NS_ENUM(NSUInteger, ICMPreviewPosition){
  
  ## More information
  
- Full documentation is available [here](http://docs.intercom.io/Install-on-your-mobile-product) and please contact 
+ Full documentation is available [here](http://docs.intercom.io/Install-on-your-mobile-product) and please contact
  us directly via Intercom for any support or questions you may have.
  
  */
@@ -123,7 +123,7 @@ typedef NS_ENUM(NSUInteger, ICMPreviewPosition){
 /*!
  If you call registerUnidentifiedUser, all activity will be tracked anonymously. If you choose to subsequently
  identify that user, all that anonymous activity will be merged into the identified user. This means that you
- will no longer see the anonymous user in Intercom, but rather the identified one. 
+ will no longer see the anonymous user in Intercom, but rather the identified one.
  
  We recommend this is called from within the application delegate's didFinishLaunchingWithOptions: method.
  
@@ -171,9 +171,9 @@ typedef NS_ENUM(NSUInteger, ICMPreviewPosition){
 /*! @name Resetting user data */
 //=========================================================================================================
 /*!
- reset is used to reset all local caches and user data the Intercom SDK has created. Use this at a time when
- you wish to log a user out of your app or change a user. Once called, the SDK will no longer communicate with Intercom
- until a further registration is made.
+ reset is used to reset all local caches and user data the Intercom SDK has created. Reset will also close
+ and active UI that is on screen. Use this at a time when you wish to log a user out of your app or change 
+ a user. Once called, the SDK will no longer communicate with Intercom until a further registration is made.
  */
 + (void)reset;
 
@@ -181,7 +181,7 @@ typedef NS_ENUM(NSUInteger, ICMPreviewPosition){
 /** @name Updating the user */
 //=========================================================================================================
 /*!
- You can send any data you like to Intercom. Typically our customers see a lot of value in sending data that 
+ You can send any data you like to Intercom. Typically our customers see a lot of value in sending data that
  relates to customer development, such as price plan, value of purchases, etc. Once these have been sent to
  Intercom you can then apply filters based on these attributes.
  
@@ -189,31 +189,31 @@ typedef NS_ENUM(NSUInteger, ICMPreviewPosition){
  
  Attributes such as the user email or name can be updated by calling
  
-     [Intercom updateUserWithAttributes:@{
-         @"email" : @"admin@intercom.io",
-         @"name" : @"Admin Name"
-     }];
+ [Intercom updateUserWithAttributes:@{
+ @"email" : @"admin@intercom.io",
+ @"name" : @"Admin Name"
+ }];
  
  Custom user attributes can be created and modified by passing a custom_attributes dictionary
  You do not have to create attributes in Intercom beforehand. If one hasn't been seen before, it will be
  created for you automatically.
  
-     [Intercom updateUserWithAttributes:@{
-         @"custom_attributes": @{
-             @"paid_subscriber" : @YES,
-             @"monthly_spend": @155.5,
-             @"team_mates": @3
-         }
-     }];
+ [Intercom updateUserWithAttributes:@{
+ @"custom_attributes": @{
+ @"paid_subscriber" : @YES,
+ @"monthly_spend": @155.5,
+ @"team_mates": @3
+ }
+ }];
  
  You can also set company data via this call by submitting an attribute dictionary like
  
-     [Intercom updateUserWithAttributes:@{
-         @"companies": @[ @{
-             @"name" : @"My Company",
-             @"id" : @"abcd1234"
-         }
-     ]}];
+ [Intercom updateUserWithAttributes:@{
+ @"companies": @[ @{
+ @"name" : @"My Company",
+ @"id" : @"abcd1234"
+ }
+ ]}];
  
  id is a required field for adding or modifying a company. A detailed description of the
  [company model is available here](https://doc.intercom.io/api/#companies-and--users)
@@ -226,9 +226,9 @@ typedef NS_ENUM(NSUInteger, ICMPreviewPosition){
 /*!
  Log an event with a given name.
  
- You can log events in Intercom based on user actions in your app. Events are different 
- to custom user attributes in that events are information on what Users did and when they 
- did it, whereas custom user attributes represent the User's current state as seen in their 
+ You can log events in Intercom based on user actions in your app. Events are different
+ to custom user attributes in that events are information on what Users did and when they
+ did it, whereas custom user attributes represent the User's current state as seen in their
  profile. See details about Events [here](http://doc.intercom.io/api/#events).
  
  @param name The name of the event that it is going to be logged.
@@ -239,13 +239,13 @@ typedef NS_ENUM(NSUInteger, ICMPreviewPosition){
  Metadata Objects support a few simple types that Intercom can present on your behalf, see the
  [Intercom API docs](http://doc.intercom.io/api/#event-metadata-types)
  
-    [Intercom logEventWithName:@"ordered_item" metaData:@{
-        @"order_date": @1392036272,
-        @"stripe_invoice": @"inv_3434343434",
-        @"order_number": @{
-        @"value": @"3434-3434",
-        @"url": @"https://example.org/orders/3434-3434"
-    }];
+ [Intercom logEventWithName:@"ordered_item" metaData:@{
+ @"order_date": @1392036272,
+ @"stripe_invoice": @"inv_3434343434",
+ @"order_number": @{
+ @"value": @"3434-3434",
+ @"url": @"https://example.org/orders/3434-3434"
+ }];
  
  @param name The name of the event you wish to track.
  @param metaData contains simple types to present to Intercom
@@ -291,7 +291,7 @@ typedef NS_ENUM(NSUInteger, ICMPreviewPosition){
 + (void)setPreviewPosition:(ICMPreviewPosition)previewPosition;
 
 /*!
- Depending on the layout of your app you may need to modify the position of the message preview relative to the 
+ Depending on the layout of your app you may need to modify the position of the message preview relative to the
  preview's position. Use this method to add sufficient padding using x and y values.
  
  @param x A horizontal padding value.
@@ -305,7 +305,8 @@ typedef NS_ENUM(NSUInteger, ICMPreviewPosition){
 
 /*!
  Use this to hide all incoming Intercom messages and message previews in the parts of your app where you do
- not wish to interrupt users, for example Camera views, parts of a game or other scenarios.
+ not wish to interrupt users, for example Camera views, parts of a game or other scenarios. If any part of the
+ SDK's UI is on screen when this is set to YES, it will close itself.
  
  @param hidden A bool that toggles message visibility. Use this to either prevent or allow messages from being
  displayed in select parts of your app.
@@ -354,9 +355,9 @@ typedef NS_ENUM(NSUInteger, ICMPresentationMode){
     ICMPresentationModeTopRight     = 3
 };
 
-/** 
-  @warning Deprecated methods will be removed in version 2.4.
-*/
+/**
+ @warning Deprecated methods will be removed in version 2.4.
+ */
 @interface Intercom (Deprecated)
 
 typedef void(^ICMCompletion)(NSError *error) __attribute((deprecated));
@@ -442,5 +443,3 @@ typedef void(^ICMCompletion)(NSError *error) __attribute((deprecated));
 + (void)registerForRemoteNotifications __attribute((deprecated("Use method 'setDeviceToken' instead.")));
 
 @end
-
-
