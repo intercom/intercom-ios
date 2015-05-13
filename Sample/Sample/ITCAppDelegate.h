a081480a0fc1eaf0d01a247f9fdfd06ce78f63e5
James Treanor <james@intercom.io>
2015-05-13 16:42:29 +0100

    Added sample app

---
+++ Sample/Sample/ITCAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ITCAppDelegate.h
+//  Sample
+//
+//  Created by James Treanor on 13/05/2015.
+//  Copyright (c) 2015 Intercom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample/Sample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Sample
+//
+//  Created by James Treanor on 13/05/2015.
+//  Copyright (c) 2015 Intercom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ITCAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ITCAppDelegate class]));
+    }
+}
