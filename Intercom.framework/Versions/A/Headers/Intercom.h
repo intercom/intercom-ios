f082f604a06265ed470b870d6a5eee04e6fa4b19
James Treanor <james@intercom.io>
2016-01-28 11:52:54 +0000

    2.3.19

--- Intercom.framework/Versions/A/Headers/Intercom.h
+++ Intercom.framework/Versions/A/Headers/Intercom.h
@@ -1,6 +1,6 @@
 //
 //  Intercom.h
-//  Intercom for iOS - Version 2.3.18
+//  Intercom for iOS - Version 2.3.19
 //
 //  Created by Intercom on 8/01/2015.
 //  Copyright (c) 2014 Intercom. All rights reserved.
@@ -10,7 +10,7 @@
 #import <UIKit/UIKit.h>
 
 #if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_7_0
-#error This version (2.3.18) of Intercom for iOS supports iOS 7.0 upwards.
+#error This version (2.3.19) of Intercom for iOS supports iOS 7.0 upwards.
 #endif
 
 NS_ASSUME_NONNULL_BEGIN
