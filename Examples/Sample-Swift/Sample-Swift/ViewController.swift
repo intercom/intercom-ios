bdd39381d3de832454114b5ce9182ca9fea029e8
Brian Boyle <brian.boyle@intercom.io>
2016-09-21 15:12:13 +0100

    Example App Update (#178)
    * Example App Update
    - Remove the dependency on Cocoapods
    - Modified the Framework search path to include Intercom.framework
    - Updated new app icons for iOS 10
    - Updated storyboard XIB for smaller devices
    
    * - Removed unneeded son files
    
    * - Updated Swift example app to Swift 3.0
    - Added photo library permission
    - Updated Swift example app with new icons and update storyboard file
    
    * Update README.md
    
    * Update README.md
    
    * - New Readme for example project
    
    * Update README.md
    
    * Update README.md

--- Examples/Sample-Swift/Sample-Swift/ViewController.swift
+++ Examples/Sample-Swift/Sample-Swift/ViewController.swift
@@ -18,54 +18,54 @@ class ViewController: UIViewController {
     @IBOutlet weak var openIntercomButton: UIButton!
     var loggedIn: Bool! = false {
         didSet {
-            loginButton.enabled = !loggedIn
-            logoutButton.enabled = loggedIn
-            openIntercomButton.enabled = loggedIn
+            loginButton.isEnabled = !loggedIn
+            logoutButton.isEnabled = loggedIn
+            openIntercomButton.isEnabled = loggedIn
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        loggedIn = NSUserDefaults.standardUserDefaults().stringForKey(emailKey) != nil
+        loggedIn = UserDefaults.standard.string(forKey: emailKey) != nil
     }
     
     
-    @IBAction func loginTapped(sender: UIButton) {
+    @IBAction func loginTapped(_ sender: UIButton) {
         //The user is not logged in, so prompt for their email address
         
-        let loginAlert = UIAlertController(title: "User Login", message: "Type in an email address", preferredStyle: UIAlertControllerStyle.Alert)
-        loginAlert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil))
-        loginAlert.addAction(UIAlertAction(title: "Login", style: UIAlertActionStyle.Default, handler: { (action: UIAlertAction) in
+        let loginAlert = UIAlertController(title: "User Login", message: "Type in an email address", preferredStyle: UIAlertControllerStyle.alert)
+        loginAlert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil))
+        loginAlert.addAction(UIAlertAction(title: "Login", style: UIAlertActionStyle.default, handler: { (action: UIAlertAction) in
             self.handleLogin(loginAlert)
         }))
-        loginAlert.addTextFieldWithConfigurationHandler { (textField : UITextField) in
+        loginAlert.addTextField { (textField : UITextField) in
             textField.placeholder = "Enter email"
         }
         loginAlert.view.setNeedsLayout()
-        self.presentViewController(loginAlert, animated: true, completion: nil)
+        self.present(loginAlert, animated: true, completion: nil)
     }
 
 
-    @IBAction func logoutTapped(sender: UIButton) {
+    @IBAction func logoutTapped(_ sender: UIButton) {
         //Logout was pressed, so calling [Intercom reset] will log remove all local user data and stop tracking them.
         Intercom.reset()
-        NSUserDefaults.standardUserDefaults().removeObjectForKey(emailKey)
+        UserDefaults.standard.removeObject(forKey: emailKey)
         loggedIn = false
     }
     
     
-    @IBAction func openIntercomTapped(sender: UIButton) {
+    @IBAction func openIntercomTapped(_ sender: UIButton) {
         Intercom.presentMessenger()
     }
     
-    func handleLogin(alertController: UIAlertController) -> Void {
+    func handleLogin(_ alertController: UIAlertController) -> Void {
         
         let textField = (alertController.textFields?.first)!
         if !(textField.text?.isEmpty)! {
             //Start tracking the user with Intercom
-            Intercom.registerUserWithEmail(textField.text!)
+            Intercom.registerUser(withEmail: textField.text!)
             
-            NSUserDefaults.standardUserDefaults().setObject(textField.text!, forKey: emailKey)
+            UserDefaults.standard.set(textField.text!, forKey: emailKey)
             loggedIn = true
         }
     }
