063b9b748a70cc96fdd57f3e69f6644d0a14d936
naren <narendra.hazareesingh@intercom.io>
2019-04-25 13:23:39 -0700

    Upgrade to Swift 5

--- Examples/Sample-Swift/Sample-Swift/AppDelegate.swift
+++ Examples/Sample-Swift/Sample-Swift/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         Intercom.setApiKey(INTERCOM_API_KEY, forAppId: INTERCOM_APP_ID)
         Intercom.setLauncherVisible(true)
         
