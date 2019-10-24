bf86ccc1e63506129d39219cfe58cf7dd240e224
Mike McNamara <mcsimumvelocity@gmail.com>
2019-10-24 16:21:10 +0100

    Updated Example Apps
    - Added new example apps that use the new `UISceneDelegate` introduced with iOS 13.
    - Updated to using `UserNotifications` for requesting permissions.
    - Updated the README.md with instructions on using the new example apps.

---
+++ Examples/Sample-Objc-UISceneDelegate/Sample-Objc-UISceneDelegate/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Sample-Objc-UISceneDelegate
+//
+//  Created by Michael McNamara on 03/10/2019.
+//  Copyright © 2019 Intercom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
