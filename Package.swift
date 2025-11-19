3de39b8609ad0c926b346ed38f73be1b11593794
Intercom iOS Release Robot <ops+intercom-ios-release-robot@intercom.com>
2025-11-19 10:41:02 +0000

    Release 19.3.4

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .binaryTarget(
             name: "Intercom",
             url: "https://github.com/intercom/intercom-ios/releases/download/19.3.4/Intercom.xcframework.zip",
-            checksum: "b87d95eabe645523f417b9051c20151ca7720c93116b163435c2ce1e87aa65ac"
+            checksum: "83f26a63832df968d0279ecdd03d8e5039ebf9bb91a9e0b1783cf184101c5a82"
         )
     ]
 )
